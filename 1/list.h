d7b713d2bb203250f9052ff53cb3ec380731556d
makabaka003 <3084608158@qq.com>
2022-03-11 12:51:40 +0800

    添加项目文件。

---
+++ 1/list.h
@@ -0,0 +1,196 @@
+﻿#ifndef LIST_H
+#define LIST_H
+#include<algorithm>
+template <typename Object>
+class List
+{
+private:
+	struct Node
+	{
+		Node *next;
+		Node *prev;
+		Object data;
+
+		Node(const Object& d = Object(), Node* n = nullptr, Node* p = nullptr) :data(d), next(n), prev(p) {}
+	};
+public:
+	class const_iterator
+	{
+	public:
+		const_iterator():current(nullptr){}
+		const Object& operator*()const { return retrieve(); }
+		const_iterator& operator++()
+		{
+			current = current->next;
+			return *this;
+		}
+		const_iterator operator++(int)
+		{
+			const_iterator old = *this;
+			++(*this);
+			return	old;
+		}
+		bool operator==(const const_iterator & rhs)const
+		{
+			return current == rhs.current;
+		}
+		bool operator!=(const const_iterator& rhs)const
+		{
+			return !(*this==rhs);
+		}
+
+	protected:
+		Node* current;
+		Object& retrieve()const { return current->data; }
+		const_iterator(Node *p):current(p){}
+		friend class List<Object>;
+	};
+	class iterator : public const_iterator
+	{
+	public:
+		iterator(){}
+		Object & operator*()
+		{
+			return const_iterator::retrieve();
+		}
+		const Object & operator*()const
+		{
+			return const_iterator::operator*();
+		}
+		iterator& operator++()
+		{
+			this->current = this->current->next;
+			return *this;
+		}
+		iterator operator++(int)
+		{
+			iterator old = *this;
+			++(*this);
+			return old;
+		}
+	protected:
+		iterator(Node *p):const_iterator(p){}
+		friend class List<Object>;
+	};
+private:
+	int theSize;
+	Node* head;
+	Node* rear;
+	void init()
+	{
+		theSize = 0;
+		head = new Node;
+		rear = new Node;
+		head->next = rear;
+		head->prev = head;
+	};
+	
+public:
+	List() 
+	{
+		init();
+	}
+	List(const List& rhs)
+	{
+		init();
+		for (auto& x : rhs)
+			push_back(x);
+	}
+	List& operator=(const List& rhs)
+	{
+		List copy = rhs;
+		std::swap(*this, copy);
+		return *this;
+	}
+	int size()const
+	{
+		return theSize;
+	}
+	bool empty()const
+	{
+		return size() == 0;
+	}
+	void clear()
+	{
+		while (!empty())
+			pop_front();
+	}
+	
+	iterator begin()
+	{
+		return (head->next);
+	}
+	const_iterator begin()const
+	{
+		return (head->next);
+	}
+	iterator end()
+	{
+		return rear;
+	}
+	const_iterator end()const
+	{
+		return rear;
+	}
+
+	Object& front()
+	{
+		return *begin();
+	}
+	const Object& front()const
+	{
+		return *begin();
+	}
+	Object& back()
+	{
+		return --end();
+	}
+	const Object& back()const
+	{
+		return --end();
+	}
+
+	iterator insert(iterator itr, const Object& x)
+	{
+		Node* p = itr.current;
+		theSize++;
+		return (p->prev = p->next = new Node(x, p->prev, p));
+	}
+	iterator erase(iterator itr)
+	{
+		Node *p = itr.current;
+		iterator retVal(p->next);
+		while (p)
+		{
+			p->prev->next = p->next;
+			p->next->prev = p->prev;
+		}
+		delete p;
+		theSize--;
+		return retVal;
+	}
+	void push_front(const Object& x)
+	{
+		insert(begin(), x);
+	}
+	void push_back(const Object& x)
+	{
+		insert(end(), x);
+	}
+	void pop_front()
+	{
+		erase(begin());
+	}
+	void pop_back()
+	{
+		erase(--end());
+	}
+	~List()
+	{
+		clear();
+		delete head;
+		delete rear;
+	}
+};
+
+#endif // LIST_H
---
+++ 1/vector.h
@@ -0,0 +1,94 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+template<typename Object>
+class Vector
+{
+public:
+	explicit Vector(int initsize = 0) :theSize(initsize), theCapacity(initsize + SPARE_CAPACITY) { objects = new Object[theCapacity]; }
+	Vector(const Vector& rhs) :theSize(rhs.theSize), theCapacity(rhs.theCapacity), objects(nullptr)
+	{
+		objects = new Object[theCapacity];
+		for (int k = 0; k < theSize; ++k)
+			objects[k] = rhs.objects[k];
+	}
+	Vector& operator=(const Vector& rhs)
+	{
+		Vector copy = rhs;
+		std::swap(*this, copy);
+		return *this;
+	}
+	~Vector()
+	{
+		delete[]objects;
+	}
+	Vector(Vector&& rhs) :theSize(rhs.theSize),
+		theCapacity(rhs.theCapacity), objects(rhs.objects) {
+		rhs.objects = nullptr;
+		rhs.theSize = 0;
+		rhs.theCapacity = 0;
+	}
+	Vector& operator=(const Vector&& rhs)
+	{
+		std::swap(theSize, rhs.theSize);
+		std::swap(theCapacity, rhs.theCapacity);
+		std::swap(objects, rhs.objects);
+		return *this;
+	}
+	void resize(int newSize) {
+		if (newSize > theCapacity)
+			reserve(newSize * 2);
+		theSize = newSize;
+	}
+	void reserve(int newCapacity) {
+		if (newCapacity < theSize)
+			return;
+		Object* newArray = new Object[newCapacity];
+		for (int k = 0; k < theSize; ++k)
+			newArray[k] = std::move(objects[k]);
+		theCapacity = newCapacity;
+		std::swap(objects, newArray);
+		delete[] newArray;
+	}
+
+	Object& operator[](int index) { return objects[index]; }
+	const Object& operator[](int index)const { return objects[index]; }
+	bool empty()const { return size() == 0; }
+	int size()const { return theSize; }
+	int capacity()const { return theCapacity; }
+
+	void push_back(const Object& x)
+	{
+		if (theSize == theCapacity)
+			reserve(2 * theCapacity + 1);
+		objects[theSize++] = x;
+	}
+	void push_back(Object&& x)
+	{
+		if (theSize == theCapacity)
+			reserve(2 * theCapacity + 1);
+		objects[theSize++] = std::move(x);
+	}
+	void pop_back()
+	{
+		--theSize;
+	}
+	const Object& back()const
+	{
+		return objects[theSize - 1];
+	}
+	typedef Object* iterator;
+	typedef const Object* const_iterator;
+
+	iterator begin() { return &objects[0]; }
+	const_iterator begin()const { return &objects[0]; }
+	iterator end() { return &objects[size()]; }
+	const_iterator end()const { return &objects[size()]; }
+
+	static const int SPARE_CAPACITY = 16;
+
+private:
+	int theSize;
+	int theCapacity;
+	Object* objects;
+};
+#endif // !VECTOR_H
