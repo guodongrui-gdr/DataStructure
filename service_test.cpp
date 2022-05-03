02927761e4d52d3a0eed0dd5f7ca879d63ef3ca7
makabaka003 <3084608158@qq.com>
2022-05-03 11:42:01 +0800

    2

--- service_test.cpp
+++ service_test.cpp
@@ -6,7 +6,9 @@ using namespace std;
 
 int main()
 {   
-    QueueSystem q(100,100,2,10);
+    QueueSystem q(480,1000,5,10);
     q.simulate();
-
+    cout << "平均等待时间为" << q.get_avr_wait_time() << endl;
+    cout << "服务顾客数为" << q.get_serviced_customer_num() << endl;
+    cout << q.get_no_service_num();
 }
