df318bbb9fd5856f6e57d5f51eb5f4d51294d298
makabaka003 <3084608158@qq.com>
2022-06-23 15:39:40 +0800

    归并排序

--- 7/mergesort.h
+++ 7/mergesort.h
@@ -1,13 +1,13 @@
 #ifndef MERGESORT_H
 #define MERGESORT_H
 
-#include<vector>
+#include<deque>
 #include<iostream>
 #include <time.h>
 #include <cstdlib>
 using namespace std;
 template<typename T>
-void merge(vector<T>& a, vector<T>& tmpArray, int leftPos, int rightPos, int rightEnd)
+void merge(deque<T>& a, deque<T>& tmpArray, int leftPos, int rightPos, int rightEnd)
 {
 	int leftEnd = rightPos - 1;
 	int tmpPos = leftPos;
@@ -32,7 +32,7 @@ void merge(vector<T>& a, vector<T>& tmpArray, int leftPos, int rightPos, int rig
 }
 
 template<typename T>
-void mergeSort(vector<T>& a, vector<T>& tmpArray, int left, int right)
+void mergesort(deque<T>& a, deque<T>& tmpArray, int left, int right)
 {
 	if (left < right)
 	{
@@ -43,29 +43,9 @@ void mergeSort(vector<T>& a, vector<T>& tmpArray, int left, int right)
 	}
 }
 template<typename T>
-void mergesort(vector<T>& a)
+void mergesort(deque<T>& a)
 {
-	vector<T> tmpArray(a.size());
+	deque<T> tmpArray(a.size());
 	mergeSort(a, tmpArray, 0, a.size() - 1);
 }
-
-int main()
-{
-	unsigned seed = 0;
-	srand(seed);
-
-	vector<int> a(1000000);
-	for (int i = 0; i < 1000000; ++i)
-	{
-		int r = rand();
-		a[i] = r;
-	}
-
-	clock_t start, end;
-	start = clock();
-	mergesort(a);
-	end = clock();
-	cout << double(end - start) / CLOCKS_PER_SEC << endl;
-}
-
 #endif // !MERGESORT_H
